b25a09feb9fb7fa50c60ecf5287307aeebebb38e
Wes Brown <wesbrown18@gmail.com>
2021-12-24 16:46:45 -0500

    Add scenario generation support.

--- go.mod
+++ go.mod
@@ -4,15 +4,19 @@ go 1.17
 
 require (
 	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d
-	github.com/wbrown/novelai-research-tool v0.0.0-20211122022657-a2c85df08669
+	github.com/wbrown/novelai-research-tool v0.0.0-20211224214033-465551dc857a
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
 
 require (
-	github.com/deckarep/golang-set v1.7.1 // indirect
+	github.com/cenkalti/backoff/v4 v4.1.2 // indirect
+	github.com/deckarep/golang-set v1.8.0 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/jdkato/prose/v2 v2.0.0 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
 	github.com/mingrammer/commonregex v1.0.1 // indirect
-	gonum.org/v1/gonum v0.7.0 // indirect
-	gopkg.in/neurosnap/sentences.v1 v1.0.6 // indirect
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	gonum.org/v1/gonum v0.9.3 // indirect
+	gopkg.in/neurosnap/sentences.v1 v1.0.7 // indirect
 )
--- nsb.go
+++ nsb.go
@@ -6,150 +6,27 @@ import (
 	"fmt"
 	uuid "github.com/nu7hatch/gouuid"
 	"github.com/wbrown/novelai-research-tool/gpt-bpe"
+	"github.com/wbrown/novelai-research-tool/scenario"
+	"github.com/wbrown/novelai-research-tool/structs"
+	"github.com/wbrown/novelai-research-tool/novelai-api"
 	"gopkg.in/yaml.v3"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
-	"reflect"
 	"strings"
 )
 
 var encoder gpt_bpe.GPTEncoder
 
-type LorebookSettings struct {
-	OrderByKeyLocations *bool `json:"orderByKeyLocations,omitempty" yaml:"orderByKeyLocations"`
-}
-
-type Lorebook struct {
-	Version    int               `json:"lorebookVersion"`
-	Entries    []LorebookEntry   `json:"entries,omitempty"`
-	Categories []Category        `json:"categories,omitempty"`
-	Settings   *LorebookSettings `json:"settings,omitempty"`
-}
-
-type ContextConfig struct {
-	Prefix               *string `json:"prefix,omitempty" yaml:"prefix"`
-	Suffix               *string `json:"suffix,omitempty" yaml:"suffix"`
-	TokenBudget          *int    `json:"tokenBudget,omitempty" yaml:"tokenBudget"`
-	ReservedTokens       *int    `json:"reservedTokens,omitempty" yaml:"reservedTokens"`
-	BudgetPriority       *int    `json:"budgetPriority,omitempty" yaml:"budgetPriority"`
-	TrimDirection        *string `json:"trimDirection,omitempty" yaml:"trimDirection"`
-	InsertionType        *string `json:"insertionType,omitempty" yaml:"insertionType"`
-	MaximumTrimType      *string `json:"maximumTrimType,omitempty" yaml:"maximumTrimType"`
-	InsertionPosition    *int    `json:"insertionPosition,omitempty" yaml:"insertionPosition"`
-	AllowInnerInsertion  *bool   `json:"allowInnerInsertion,omitempty" yaml:"allowInnerInsertion"`
-	AllowInsertionInside *bool   `json:"allowInsertionInside,omitempty" yaml:"allowInsertionInside"`
-	Force                *bool   `json:"forced,omitempty" yaml:"forced"`
-}
-
-const (
-	BiasString    BiasType = 0
-	BiasTokens             = 1
-	BiasLitString          = 2
-)
-
-type BiasSequences struct {
-	Sequences []gpt_bpe.Tokens `json:"sequences"`
-	Type      BiasType         `json:"type"`
-}
-
-type LoreBiasGroup struct {
-	YamlPhrases          *[]string        `json:"-" yaml:"phrases"`
-	Phrases              *[]BiasSequences `json:"phrases,omitempty" yaml:"-"`
-	Bias                 *float64         `json:"bias,omitempty" yaml:"bias"`
-	EnsureSequenceFinish *bool            `json:"ensure_sequence_finish,omitempty" yaml:"ensureSequenceFinish"`
-	GenerateOnce         *bool            `json:"generate_once,omitempty" yaml:"generateOnce"`
-	Enabled              *bool            `json:"enabled,omitempty" yaml:"enabled"`
-	WhenInactive         *bool            `json:"whenInactive,omitempty" yaml:"whenInactive"`
-}
-
-type LoreBiasGroups []LoreBiasGroup
-
-type LorebookEntry struct {
-	Text                *string         `json:"text,omitempty" yaml:"text"`
-	ContextCfg          *ContextConfig  `json:"contextConfig,omitempty" yaml:"contextConfig"`
-	LastUpdatedAt       *int            `json:"lastUpdatedAt,omitempty" yaml:"lastUpdatedAt"`
-	DisplayName         *string         `json:"displayName,omitempty" yaml:"displayName"`
-	Keys                *[]string       `json:"keys,omitempty" yaml:"keys"`
-	SearchRange         *int            `json:"searchRange,omitempty" yaml:"searchRange"`
-	Enabled             *bool           `json:"enabled,omitempty" yaml:"enabled"`
-	ForceActivation     *bool           `json:"forceActivation,omitempty" yaml:"forceActivation"`
-	KeyRelative         *bool           `json:"keyRelative,omitempty" yaml:"keyRelative"`
-	NonStoryActivatable *bool           `json:"nonStoryActivatable,omitempty" yaml:"nonStoryActivatable"`
-	CategoryId          *string         `json:"category,omitempty" yaml:"categoryId"`
-	LoreBiasGroups      *LoreBiasGroups `json:"loreBiasGroups,omitempty" yaml:"loreBiasGroups"`
-}
-
-type BiasType uint
-
-type Category struct {
-	Name                *string         `json:"name,omitempty" yaml:"name"`
-	Id                  *string         `json:"id,omitempty" yaml:"id"`
-	Enabled             *bool           `json:"enabled,omitempty" yaml:"enabled"`
-	CreateSubcontext    *bool           `json:"createSubcontext,omitempty" yaml:"createSubcontext"`
-	SubcontextSettings  *LorebookEntry  `json:"subcontextSettings,omitempty" yaml:"subcontextSettings"`
-	UseCategoryDefaults *bool           `json:"useCategoryDefaults,omitempty" yaml:"useCategoryDefaults"`
-	CategoryDefaults    *LorebookEntry  `json:"categoryDefaults,omitempty" yaml:"categoryDefaults"`
-	LoreBiasGroups      *LoreBiasGroups `json:"loreBiasGroups,omitempty" yaml:"loreBiasGroups"`
-}
-
-type Definition struct {
-	LorebookSettings *LorebookSettings `yaml:"lorebookSettings"`
-	Categories       map[string]*Category
-	Lorebook         []struct {
-		Category *string
-		Config   *LorebookEntry
-		Entries  map[string]LorebookEntry
-	}
-}
-
-type CategoriesMap map[string]*Category
-
-func (defaults *LorebookEntry) RealizeDefaults(entry *LorebookEntry) {
-	fields := reflect.TypeOf(*defaults)
-	for field := 0; field < fields.NumField(); field++ {
-		fieldValues := reflect.ValueOf(defaults).Elem().Field(field)
-		if fieldValues.IsNil() {
-			continue
-		}
-		entryValue := reflect.ValueOf(entry).Elem().Field(field)
-		if entryValue.IsNil() {
-			entryValue.Set(fieldValues)
-		}
-	}
-}
-
-func (biasGroups *LoreBiasGroups) RealizeBiases() {
-	for biasIdx := range *biasGroups {
-		biasGroup := (*biasGroups)[biasIdx]
-		if biasGroup.YamlPhrases != nil {
-			if (*biasGroups)[biasIdx].Phrases == nil {
-				biasSequences := make([]BiasSequences, 0)
-				(*biasGroups)[biasIdx].Phrases = &biasSequences
-			}
-			for phraseIdx := range *biasGroup.YamlPhrases {
-				jsonifiedPhrase := BiasSequences{
-					Sequences: make([]gpt_bpe.Tokens, 0),
-					Type:      BiasLitString,
-				}
-				phraseString := (*biasGroup.YamlPhrases)[phraseIdx]
-				tokens := encoder.Encode(&phraseString)
-				jsonifiedPhrase.Sequences = append(jsonifiedPhrase.Sequences,
-					*tokens)
-				*(*biasGroups)[biasIdx].Phrases = append(
-					*(*biasGroups)[biasIdx].Phrases, jsonifiedPhrase)
-			}
-		}
-	}
-}
-
-func (categories *CategoriesMap) RealizeCategory(name string, category *Category) *Category {
+type CategoriesMap map[string]*scenario.Category
+func (categories *CategoriesMap) RealizeCategory(name string,
+	category *scenario.Category) *scenario.Category {
 	if lookup, ok := (*categories)[name]; ok {
 		return lookup
 	}
 	if category == nil {
-		newCategory := Category{
+		newCategory := scenario.Category{
 			Name: &name,
 		}
 		category = &newCategory
@@ -169,15 +46,152 @@ func (categories *CategoriesMap) RealizeCategory(name string, category *Category
 	return category
 }
 
-func (def *Definition) RealizeDefinition(categories *CategoriesMap) {
-	for categoryKey := range def.Categories {
-		categories.RealizeCategory(categoryKey, def.Categories[categoryKey])
+type Definition struct {
+	Title             *string                    `yaml:"title"`
+	Description       *string                    `yaml:"description"`
+	Tags              *[]string                  `yaml:"tags"`
+	Prompt            *string                    `yaml:"prompt"`
+	Memory            *string                    `yaml:"memory"`
+	AuthorsNote       *string                    `yaml:"authorsNote"`
+	MemoryConfig      *scenario.ContextConfig    `yaml:"memoryConfig"`
+	AuthorsNoteConfig *scenario.ContextConfig    `yaml:"authorsNoteConfig"`
+	StoryConfig       *scenario.ContextConfig    `yaml:"storyConfig"`
+	LorebookSettings  *scenario.LorebookSettings `yaml:"lorebookSettings"`
+	Placeholders      *scenario.Placeholders     `yaml:"placeholders"`
+	Categories        *CategoriesMap             `yaml:"categories"`
+	Biases            *structs.BiasGroups        `yaml:"biases"`
+	Lorebook          []struct {
+		Category *string
+		Config   *scenario.LorebookEntry
+		Entries  map[string]scenario.LorebookEntry
+	}
+}
+
+func RealizeScenario(sc *scenario.Scenario, inputFiles []string) {
+	for fileIdx := range inputFiles {
+		fileName := inputFiles[fileIdx]
+		data, err := ioutil.ReadFile(fileName)
+		if err != nil {
+			log.Fatal(fmt.Sprintf("Error reading %s:\n%v", fileName, err))
+		}
+		defs := Definition{}
+		if err := yaml.Unmarshal(data, &defs); err != nil {
+			log.Fatal(fmt.Sprintf("Error processing %s:\n%v", fileName, err))
+		}
+		if defs.Title != nil {
+			if sc.Title != "" {
+				log.Printf("WARNING: Scenario title already set! "+
+					"Overwriting with '%s'.", *defs.Title)
+			}
+			sc.Title = *defs.Title
+		}
+		if defs.Description != nil {
+			if sc.Title != "" {
+				log.Printf("WARNING: Scenario description already"+
+					"set! Overwriting with '%s'.", *defs.Description)
+			}
+			sc.Description = *defs.Description
+		}
+		if defs.Prompt != nil {
+			if sc.Prompt != "" {
+				log.Printf("WARNING: Prompt already set! Overwriting.")
+			}
+			sc.Prompt = *defs.Prompt
+		}
+		if defs.Memory != nil {
+			if sc.Context[0].Text != nil {
+				log.Printf("WARNING: Memory already set! Overwriting.")
+			}
+			sc.Context[0].Text = defs.Memory
+		}
+		if defs.AuthorsNote != nil {
+			if sc.Context[1].Text != nil {
+				log.Printf("WARNING: Author's Note already set! " +
+					"Overwriting.")
+			}
+			sc.Context[1].Text = defs.AuthorsNote
+		}
+		if defs.Tags != nil {
+			for tagIdx := range *defs.Tags {
+				newTag := (*defs.Tags)[tagIdx]
+				exists := false
+				for oldTagsIdx := range sc.Tags {
+					if sc.Tags[oldTagsIdx] == newTag {
+						exists = true
+						break
+					}
+				}
+				if !exists {
+					sc.Tags = append(sc.Tags, newTag)
+				}
+			}
+		}
+		if defs.MemoryConfig != nil {
+			if len(sc.Context) < 1 {
+				sc.Context = append(sc.Context,
+					scenario.ContextEntry{})
+			}
+			
+			if sc.Context[0].ContextCfg != nil {
+				log.Printf("WARNING: memoryConfig already set! " +
+					"Overwriting.")
+			}
+			sc.Context[0].ContextCfg = defs.MemoryConfig
+		}
+		if defs.AuthorsNoteConfig != nil {
+			if sc.Context[1].ContextCfg != nil {
+				log.Printf("WARNING: authorsNoteConfig already set! " +
+					"Overwriting.")
+			}
+			sc.Context[1].ContextCfg = defs.AuthorsNoteConfig
+		}
+		if defs.StoryConfig != nil {
+			if sc.StoryContextConfig != nil {
+				log.Printf("WARNING: authorsNoteConfig already set! " +
+					"Overwriting.")
+			}
+			sc.StoryContextConfig = defs.StoryConfig
+		}
+		if defs.Placeholders != nil {
+			if sc.PlaceholderMap == nil {
+				sc.PlaceholderMap = make(scenario.Placeholders, 0)
+			}
+			sc.PlaceholderMap.Add(*defs.Placeholders)
+		}
+		if defs.Biases != nil {
+			defs.Biases.RealizeBiases()
+			if sc.Settings.Parameters.LogitBiasGroups == nil {
+				sc.Settings.Parameters.LogitBiasGroups = defs.Biases
+			} else {
+				*sc.Settings.Parameters.LogitBiasGroups = 
+					append(*sc.Settings.Parameters.LogitBiasGroups,
+					*defs.Biases...)
+			}
+		}
+		if len(sc.PlaceholderMap) > 0 {
+			sc.PlaceholderMap.Realize()
+			sc.Placeholders = make([]scenario.Placeholder, 0)
+			for key := range sc.PlaceholderMap {
+				sc.Placeholders = append(sc.Placeholders,
+					*sc.PlaceholderMap[key])
+			}
+		}
+	}
+}
+
+func (def *Definition) RealizeLorebookDefs(categories *CategoriesMap) {
+	if def.Categories == nil {
+		def.Categories = &CategoriesMap{}
+	}
+	for categoryKey := range *def.Categories {
+		categories.RealizeCategory(categoryKey, (*def.Categories)[categoryKey])
 	}
 	for lorebookGroupIdx := range def.Lorebook {
 		lorebookGroup := def.Lorebook[lorebookGroupIdx]
-		var category *Category
+		var category *scenario.Category
 		if lorebookGroup.Category != nil {
-			category = categories.RealizeCategory(*lorebookGroup.Category, nil)
+			category = categories.RealizeCategory(*lorebookGroup.Category, 
+				nil)
 		}
 		for entryKey := range lorebookGroup.Entries {
 			entry := lorebookGroup.Entries[entryKey]
@@ -199,19 +213,21 @@ func (def *Definition) RealizeDefinition(categories *CategoriesMap) {
 }
 
 func (def *Definition) ToJson(categories *CategoriesMap) []byte {
-	lorebook := Lorebook{
+	lorebook := scenario.Lorebook{
 		Version:    3,
-		Entries:    make([]LorebookEntry, 0),
-		Categories: make([]Category, 0),
+		Entries:    make([]scenario.LorebookEntry, 0),
+		Categories: make([]scenario.Category, 0),
 	}
 	for lorebookGroupIdx := range def.Lorebook {
 		lorebookGroup := def.Lorebook[lorebookGroupIdx]
 		for entryName := range lorebookGroup.Entries {
-			lorebook.Entries = append(lorebook.Entries, lorebookGroup.Entries[entryName])
+			lorebook.Entries = append(lorebook.Entries,
+				lorebookGroup.Entries[entryName])
 		}
 	}
 	for categoryKey := range *categories {
-		lorebook.Categories = append(lorebook.Categories, *(*categories)[categoryKey])
+		lorebook.Categories = append(lorebook.Categories,
+			*(*categories)[categoryKey])
 	}
 	jsonBytes, err := json.MarshalIndent(lorebook, "", " ")
 	if err != nil {
@@ -220,27 +236,47 @@ func (def *Definition) ToJson(categories *CategoriesMap) []byte {
 	return jsonBytes
 }
 
-func (lorebook *Lorebook) ToPlaintext() string {
-	entryStrings := make([]string, 0)
-	for entryIdx := range lorebook.Entries {
-		entry := lorebook.Entries[entryIdx]
-		if entry.Enabled == nil || *entry.Enabled {
-			normalizedDisplayName := strings.Replace(*entry.DisplayName, ":", " -", -1)
-			entryStrings = append(entryStrings,
-				strings.Join([]string{normalizedDisplayName, *entry.Text}, ":\n"))
-		}
-	}
-	return strings.Join(entryStrings, "\n***\n")
-}
-
 func init() {
 	encoder = gpt_bpe.NewEncoder()
 }
 
+func RealizeLorebook(lorebook *scenario.Lorebook, categories *CategoriesMap,
+	inputFiles []string) {
+	for fileIdx := range inputFiles {
+		fileName := inputFiles[fileIdx]
+		data, err := ioutil.ReadFile(fileName)
+		if err != nil {
+			log.Fatal(fmt.Sprintf("Error reading %s:\n%v", fileName, err))
+		}
+		defs := Definition{}
+		if err := yaml.Unmarshal(data, &defs); err != nil {
+			log.Fatal(fmt.Sprintf("Error processing %s:\n%v", fileName, err))
+		}
+		defs.RealizeLorebookDefs(categories)
+		for lorebookGroupIdx := range defs.Lorebook {
+			lorebookGroup := defs.Lorebook[lorebookGroupIdx]
+			for entryName := range lorebookGroup.Entries {
+				lorebook.Entries = append(lorebook.Entries,
+					lorebookGroup.Entries[entryName])
+			}
+		}
+		if defs.LorebookSettings != nil {
+			lorebook.Settings = *defs.LorebookSettings
+		}
+	}
+	for categoryKey := range *categories {
+		category := (*categories)[categoryKey]
+		lorebook.Categories = append(lorebook.Categories,
+			*category)
+	}
+}
+
+
+
 func main() {
 	var outputFile string
 	var plaintext bool
-	flag.StringVar(&outputFile, "o", "output.lorebook", "output lorebook filename")
+	flag.StringVar(&outputFile, "o", "output", "output base filename")
 	flag.BoolVar(&plaintext, "p", false, "plaintext output")
 	flag.Parse()
 	inputFileArgs := flag.Args()
@@ -261,46 +297,35 @@ func main() {
 		inputFiles = append(inputFiles, filePaths...)
 	}
 	categories := make(CategoriesMap, 0)
-	lorebook := Lorebook{
+	lorebook := scenario.Lorebook{
 		Version: 3,
 	}
 
-	for fileIdx := range inputFiles {
-		fileName := inputFiles[fileIdx]
-		data, err := ioutil.ReadFile(fileName)
-		if err != nil {
-			log.Fatal(fmt.Sprintf("Error reading %s:\n%v", fileName, err))
-		}
-		defs := Definition{}
-		if err := yaml.Unmarshal(data, &defs); err != nil {
-			log.Fatal(fmt.Sprintf("Error processing %s:\n%v", fileName, err))
-		}
-		defs.RealizeDefinition(&categories)
-		for lorebookGroupIdx := range defs.Lorebook {
-			lorebookGroup := defs.Lorebook[lorebookGroupIdx]
-			for entryName := range lorebookGroup.Entries {
-				lorebook.Entries = append(lorebook.Entries, lorebookGroup.Entries[entryName])
-			}
-		}
-		if defs.LorebookSettings != nil {
-			lorebook.Settings = defs.LorebookSettings
-		}
+	RealizeLorebook(&lorebook, &categories, inputFiles)
+	scenario := scenario.Scenario{
+		ScenarioVersion: 1,
+		Lorebook: lorebook,
+		Context: []scenario.ContextEntry{
+			scenario.ContextEntry{},
+			scenario.ContextEntry{}},
+		Settings: scenario.ScenarioSettings{
+			Parameters: &novelai_api.NaiGenerateParams{},
+		},
 	}
-	for categoryKey := range categories {
-		lorebook.Categories = append(lorebook.Categories, *(categories)[categoryKey])
-	}
+	RealizeScenario(&scenario, inputFiles)
+
 	var outputBytes []byte
 	if plaintext {
-		text := lorebook.ToPlaintext()
-		outputBytes = []byte(text)
-	} else {
-		var err error
-		outputBytes, err = json.MarshalIndent(lorebook, "", " ")
-		if err != nil {
-			log.Fatal(err)
-		}
+		lorebook.ToPlaintextFile(outputFile + ".txt")
 	}
-	if err := ioutil.WriteFile(outputFile, outputBytes, 0755); err != nil {
+	lorebook.ToFile(outputFile + ".lorebook")	
+	var err error
+	outputBytes, err = json.MarshalIndent(scenario, "", " ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := ioutil.WriteFile(outputFile + ".scenario", outputBytes, 
+		0755); err != nil {
 		log.Fatal(err)
 	}
 }
